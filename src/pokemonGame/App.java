9755a4dcf96eb9a66bbac43d076994d5ee7b134a
Joel Hubbard <joeltylerhubbard@fastmail.com>
2026-03-03 09:09:37 -0500

    Fixed type effectiveness logic

--- src/pokemonGame/App.java
+++ src/pokemonGame/App.java
@@ -20,7 +20,7 @@ public class App {
             float effectiveness = attack.calculateEffectiveness(gengar, move);
             System.out.println(pikachu.getName() + " used " + move.getMoveName() + " against " + gengar.getName() + "!");
             System.out.println(move.getMoveName() + " is a " + move.getType() + " type move and " + gengar.getName() + " is a " + gengar.getTypePrimary() + "/" + gengar.getTypeSecondary() + " type Pokemon.");
-            System.out.println("It's " + (effectiveness > 1 ? "super effective!" : effectiveness < 1 ? "not very effective..." : effectiveness == 0.0 ? "It had no effect..." : "effective."));
+            System.out.println("It" + (effectiveness == 0.0 ? " had no effect..." : effectiveness < 1 ? "'s not very effective..." : effectiveness > 1 ? "'s super effective!" : "'s effective."));
             System.out.println("Damage multiplier: " + effectiveness);
             System.out.println();
         }

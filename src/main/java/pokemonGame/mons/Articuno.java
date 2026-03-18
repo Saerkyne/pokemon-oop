8c79bec8afd429bf533bb6847067547c78f20464
Joel Hubbard <joeltylerhubbard@fastmail.com>
2026-03-18 13:29:58 -0400

    Added full list of moves to file for TM/HM to make setting learnsets easier

--- src/main/java/pokemonGame/mons/Articuno.java
+++ src/main/java/pokemonGame/mons/Articuno.java
@@ -1,5 +1,6 @@
 ﻿package pokemonGame.mons;
 import pokemonGame.Pokemon;
+import pokemonGame.moves.*;
 import pokemonGame.LearnsetEntry;
 import java.util.List;
 
@@ -7,7 +8,22 @@ public class Articuno extends Pokemon {
 
     private static final List<LearnsetEntry> LEARNSET = new java.util.ArrayList<>();
     static {
-        // Learnset will be populated manually later
+        // Level up moves
+        LEARNSET.add(new LearnsetEntry(new IceBeam(), LearnsetEntry.Source.LEVEL, 1));
+        LEARNSET.add(new LearnsetEntry(new Peck(), LearnsetEntry.Source.LEVEL, 1));
+        LEARNSET.add(new LearnsetEntry(new Blizzard(), LearnsetEntry.Source.LEVEL, 51));
+        LEARNSET.add(new LearnsetEntry(new Agility(), LearnsetEntry.Source.LEVEL, 55));
+        LEARNSET.add(new LearnsetEntry(new Mist(), LearnsetEntry.Source.LEVEL, 60));
+
+        // HM moves
+        LEARNSET.add(new LearnsetEntry(new Fly(), LearnsetEntry.Source.HM, 2));
+
+        // TM Moves
+        LEARNSET.add(new LearnsetEntry(new RazorWind(), LearnsetEntry.Source.TM, 2));
+        LEARNSET.add(new LearnsetEntry(new Whirlwind(), LearnsetEntry.Source.TM, 4));
+        LEARNSET.add(new LearnsetEntry(new Toxic(), LearnsetEntry.Source.TM, 6));
+        LEARNSET.add(new LearnsetEntry(new HyperBeam(), LearnsetEntry.Source.TM, 15));
+        
     }
 
     public Articuno(String name) {

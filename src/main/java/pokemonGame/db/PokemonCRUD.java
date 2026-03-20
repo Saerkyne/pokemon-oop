91b5c886f7a2bde99cbad66edd8019d163a57a1b
Joel Hubbard <cal@calabriel.com>
2026-03-20 15:00:10 -0400

    Refactor Pokémon CRUD operations to use unified table structure and improve SQL queries

--- src/main/java/pokemonGame/db/PokemonCRUD.java
+++ src/main/java/pokemonGame/db/PokemonCRUD.java
@@ -14,10 +14,7 @@
 // should be kept in the code, not the database.   
 
 package pokemonGame.db;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PokemonCRUD {
     public static void main(String[] args) {
@@ -26,11 +23,12 @@ public class PokemonCRUD {
             // Create a trainer and some Pokémon for testing
             try (Statement stmt = conn.createStatement()) {
                 stmt.executeUpdate(
-                    "INSERT INTO trainers (name) VALUES ('Red')"
+                    "INSERT INTO trainers (discord_id, discord_username, name) "
+                    + "VALUES (123456789012345678, 'Calabriel', 'Red')"
                 );
                 // Get the ID of the newly created trainer
                 ResultSet rs = stmt.executeQuery(
-                    "SELECT trainer_id FROM trainers WHERE name = 'Red'"
+                    "SELECT trainer_id FROM trainers WHERE discord_id = 123456789012345678"
                 );
                 // Assuming the trainer was created successfully, we should have a result
                 rs.next();
@@ -40,50 +38,57 @@ public class PokemonCRUD {
                 System.out.println("Created trainer 'Red' with ID: " + redId);
                 // Now we can create some Pokémon for 'Red' using the retrieved trainer ID
                 stmt.executeUpdate(
-                    "INSERT INTO pokemon (species, name, dex_index, type_primary, type_secondary, "
-                    + "level, base_hp, base_attack, base_defense, base_sp_attack, base_sp_defense, "
-                    + "base_speed, trainer_id) VALUES "
-                    + "('Bulbasaur', 'Bulby', 1, 'Grass', 'Poison', 5, 45, 49, 49, 65, 65, 45, " + redId + ")"
+                    "INSERT INTO pokemon_instances "
+                    + "(trainer_id, species, nickname, level, nature, "
+                    + "iv_hp, iv_attack, iv_defense, iv_sp_attack, iv_sp_defense, iv_speed, "
+                    + "current_hp) VALUES "
+                    + "(" + redId + ", 'Bulbasaur', 'Bulby', 5, 'Adamant', "
+                    + "28, 31, 14, 22, 19, 25, "
+                    + "21)"
                 );
 
                 stmt.executeUpdate(
-                    "INSERT INTO pokemon (species, name, dex_index, type_primary, type_secondary, "
-                    + "level, base_hp, base_attack, base_defense, base_sp_attack, base_sp_defense, "
-                    + "base_speed, trainer_id) VALUES "
-                    + "('Charmander', 'Charmander', 4, 'Fire', NULL, 5, 39, 52, 43, 60, 50, 65, " + redId + ")"
+                    "INSERT INTO pokemon_instances "
+                    + "(trainer_id, species, nickname, level, nature, "
+                    + "iv_hp, iv_attack, iv_defense, iv_sp_attack, iv_sp_defense, iv_speed, "
+                    + "current_hp) VALUES "
+                    + "(" + redId + ", 'Charmander', 'Charmander', 5, 'Jolly', "
+                    + "15, 22, 18, 27, 11, 30, "
+                    + "19)"
                 );
             }
 
             try (Statement stmt = conn.createStatement()) {
                 ResultSet rs = stmt.executeQuery(
-                  "SELECT p.name, p.species, p.level, p.type_primary, p.type_secondary, "
-                  + "FROM Pokemon p "
-                  + "JOIN trainers t ON p.trainer_id = t.trainer_id "
-                  + "WHERE t.name = 'Red'"
+                    "SELECT pi.nickname, pi.species, pi.level, pi.nature, pi.current_hp "
+                    + "FROM pokemon_instances pi "
+                    + "JOIN trainers t ON pi.trainer_id = t.trainer_id "
+                    + "WHERE t.name = 'Red'"
                 );
 
-                System.out.println("\nRed's Team:");
-                while(rs.next()) {
-                    String name = rs.getString("name");
-                    String species = rs.getString("species");
-                    int level = rs.getInt("level");
-                    String typePrimary = rs.getString("type_primary");
-                    String typeSecondary = rs.getString("type_secondary"); //may be null
-                    String typeStr = typeSecondary != null ? typePrimary + "/" + typeSecondary : typePrimary;
-                    System.out.printf("  Lv. %d %s (%s) [%s]%n", level, name, species, typeStr);
+                System.out.println("\nRed's team:");
+                while (rs.next()) {
+                    System.out.printf("  Lv.%d %s (%s) [%s] HP: %d%n",
+                            rs.getInt("level"),
+                            rs.getString("nickname"),
+                            rs.getString("species"),
+                            rs.getString("nature"),
+                            rs.getInt("current_hp"));
                 }
             }
 
             try (Statement stmt = conn.createStatement()) {
                 int rows = stmt.executeUpdate(
-                    "UPDATE pokemon SET level = 6 WHERE species = 'Bulbasaur' AND name = 'Bulby'"
+                    "UPDATE pokemon_instances SET level = 6 "
+                    + "WHERE species = 'Bulbasaur' AND nickname = 'Bulby'"
                 );
-                System.out.println("\nUpdated " + rows + " row(s) - Bulby leveled up to 6!");
+                System.out.println("\nUpdated " + rows + " row(s) — Bulby leveled up to 6!");
             }
 
             try (Statement stmt = conn.createStatement()) {
                 int rows = stmt.executeUpdate(
-                    "DELETE FROM pokemon WHERE species = 'Charmander' AND name = 'Charmander'"
+                    "DELETE FROM pokemon_instances "
+                    + "WHERE species = 'Charmander' AND nickname = 'Charmander'"
                 );
                 System.out.println("Deleted " + rows + " row(s) — Charmander was released.");
             }
